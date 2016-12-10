2e495da7a40edfdb05b64a576085a524c50517a4
nguyenxuanluan <luannxse04649@fpt.edu.vn>
2016-12-10 12:54:16 +0700

    …

--- Test/Stage6.swift
+++ Test/Stage6.swift
@@ -2,7 +2,7 @@
 //  Stage6.swift
 //  Test
 //
-//  Created by Admin on 12/10/16.
+//  Created by LuanNX on 12/10/16.
 //  Copyright © 2016 LuanNX. All rights reserved.
 //
 
@@ -10,12 +10,11 @@ import Foundation
 import SpriteKit
 class Stage6 : Frame {
     
-   override func configAction() {
+    override func configAction() {
         view.anchorPoint=CGPoint(x: 0.5, y: 0.5)
         addEnemy2(position: CGPoint(x: 0, y: 0),startPoint: CGPoint(x: -120, y: 0), endPoint: CGPoint(x: +200, y: 0))
-        addEnemy2(position: CGPoint(x: 0, y: 0),startPoint: CGPoint(x: +120, y: 0), endPoint: CGPoint(x: -200, y: 0))
-
-  
+        addEnemy2(position: CGPoint(x: 0, y: 0),startPoint: CGPoint(x: 120, y: 0), endPoint: CGPoint(x: -200, y: 0))
+        
     }
     func addEnemy2(position: CGPoint,startPoint: CGPoint, endPoint: CGPoint){
         let enemy=EnemyController(texture: Textures.STICK ,resize: 8)
